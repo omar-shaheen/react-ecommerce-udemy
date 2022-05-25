1860ffed226b90e8ad0be4d5e7392e04ae63c6c9
Omar Shaheen <16275929+omar-shaheen@users.noreply.github.com>
2022-05-25 02:20:09 +0200

    Reactjs

---
+++ react-app-project/src/components/TopNavbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "../assets/css/custom.css"
+
+export class TopNavbar extends Component {
+  render() {
+    return (
+      <div>TopNavbar</div>
+    )
+  }
+}
+
+export default TopNavbar
